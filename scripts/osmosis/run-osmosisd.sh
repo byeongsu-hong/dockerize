06c8df24eebb1367a201bb36b93c419897f6454f
frostornge <frostornge@gmail.com>
2022-05-04 16:19:50 +0000

    enable prometheus

--- scripts/osmosis/run-osmosisd.sh
+++ scripts/osmosis/run-osmosisd.sh
@@ -3,6 +3,10 @@
 MONIKER=${MONIKER:-moniker}
 OSMOSIS_HOME=${OSMOSIS_HOME:-$HOME/.osmosisd}
 
+# apply custom moniker
 sed -i "s/moniker = \"eddy\"/moniker = \"$MONIKER\"/g" "$OSMOSIS_HOME"/config/config.toml
+
+# enable prometheus
+sed -i "s/prometheus = false/prometheus = true/g" "$OSMOSIS_HOME"/config/config.toml
 
 osmosisd start --home "$OSMOSIS_HOME"
