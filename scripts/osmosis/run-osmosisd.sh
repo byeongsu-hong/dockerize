564a8d74adc0d7e2d60636c8872bcba30ae3d00d
frostornge <frostornge@gmail.com>
2022-05-06 15:17:06 +0000

    disable indexer

--- scripts/osmosis/run-osmosisd.sh
+++ scripts/osmosis/run-osmosisd.sh
@@ -6,6 +6,9 @@ OSMOSIS_HOME=${OSMOSIS_HOME:-$HOME/.osmosisd}
 # apply custom moniker
 sed -i "s/moniker = \"eddy\"/moniker = \"$MONIKER\"/g" "$OSMOSIS_HOME"/config/config.toml
 
+# disable indexer
+sed -i "s/indexer = \"kv\"/indexer = \"null\"/g" "$OSMOSIS_HOME"/config/config.toml
+
 # enable prometheus
 sed -i "s/prometheus = false/prometheus = true/g" "$OSMOSIS_HOME"/config/config.toml
 
