365e70df8bf7a02096ea13791338db3bfc1f8cf2
frostornge <frostornge@gmail.com>
2022-05-09 05:11:15 +0000

    cors all

--- scripts/osmosis/run-osmosisd.sh
+++ scripts/osmosis/run-osmosisd.sh
@@ -12,6 +12,9 @@ sed -i "s/indexer = \"kv\"/indexer = \"null\"/g" "$OSMOSIS_HOME"/config/config.t
 # enable prometheus
 sed -i "s/prometheus = false/prometheus = true/g" "$OSMOSIS_HOME"/config/config.toml
 
+# CORS
+sed -i "s/cors_allowed_origins = []/cors_allowed_origins = [\"*\"]/g" "$OSMOSIS_HOME"/config/config.toml
+
 # prune everything
 sed -i "s/pruning = \"custom\"/pruning = \"everything\"/g" "$OSMOSIS_HOME"/config/app.toml
 
